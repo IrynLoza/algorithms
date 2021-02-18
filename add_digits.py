766c9a67baf341edf5121d5148915dba14b00c95
Iryna Loza <irynloza@gmail.com>
2021-02-18 11:41:33 -0800

    Initial commit

---
+++ add_digits.py
@@ -0,0 +1,21 @@
+"""Given a non-negative integer num, repeatedly add all its digits until the
+result has only one digit:
+>>> 38
+2
+>>> 58
+4 
+>>> 199
+1
+"""
+
+def add_digits(num):
+    """Return one digit after repeatedly add"""
+    pass
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n ALL TESTS PASS! AWESOME JOB Young Padawan \n')
