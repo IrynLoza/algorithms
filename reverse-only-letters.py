b5bb5a973b850c8d22e27c289f112df53c171354
Iryna Loza <irynloza@gmail.com>
2021-02-18 18:18:47 -0800

    Initial commit

---
+++ reverse-only-letters.py
@@ -0,0 +1,18 @@
+"""Given a string S, return the "reversed" string where all characters that are not a 
+letter stay in the same place, and all letters reverse their positions:
+>>> reverseOnlyLetters("ab-cd")
+"dc-ba"
+>>> reverseOnlyLetters("a-bC-dEf-ghIj")
+"j-Ih-gfE-dCba"
+"""
+
+def reverseOnlyLetters(S):
+    """Return reversed letters only"""
+    pass
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n ALL TESTS PASS! YOU DID IT AGAIN! FORCE WITH YOU!')
