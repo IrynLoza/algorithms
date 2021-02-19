7a72039732883187020518865bdfa87393070a72
Iryna Loza <irynloza@gmail.com>
2021-02-18 18:20:39 -0800

    Update reverseOnlyLetters

--- reverse-only-letters.py
+++ reverse-only-letters.py
@@ -1,9 +1,9 @@
 """Given a string S, return the "reversed" string where all characters that are not a 
 letter stay in the same place, and all letters reverse their positions:
 >>> reverseOnlyLetters("ab-cd")
-"dc-ba"
+'dc-ba'
 >>> reverseOnlyLetters("a-bC-dEf-ghIj")
-"j-Ih-gfE-dCba"
+'j-Ih-gfE-dCba'
 """
 
 def reverseOnlyLetters(S):
