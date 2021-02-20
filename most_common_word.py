6e800a6a042f704678189f9ca670e163cf8b23e2
Iryna Loza <irynloza@gmail.com>
2021-02-19 20:01:01 -0800

    Complete most_common_word

--- most_common_word.py
+++ most_common_word.py
@@ -1,9 +1,10 @@
+import string
 """Given a paragraph and a list of banned words, return the most frequent word that is not 
 in the list of banned words.  It is guaranteed there is at least one word that isn't banned, 
 and that the answer is unique.
 
 Words in the list of banned words are given in lowercase, and free of punctuation.  Words in 
-the paragraph are not case sensitive.  The answer is in lowercase.
+the paragraph are not case sensitive.  The answer is in lowercase:
 >>> most_common_word("Bob hit a ball, the hit BALL flew far after it was hit.", ["hit"])
 'ball'
 >>> most_common_word("Bob. hIt, baLl", ["bob", "hit"])
@@ -13,7 +14,34 @@ the paragraph are not case sensitive.  The answer is in lowercase.
 """
 
 def most_common_word(paragraph, banned):
-    pass
+    """Return the most frequent word that is not in the list of banned words"""
+    
+    paragraph = paragraph.lower()
+        
+    #Remove punctuation
+    exclude = set(string.punctuation)
+    new_paragraph = ''
+    devicer = ' '
+    for char in paragraph:
+        if char not in exclude:
+            new_paragraph = f'{new_paragraph}{char}'
+        else:
+            new_paragraph = f'{new_paragraph}{devicer}'
+        
+    new_paragraph = new_paragraph.split(' ')
+
+    words = {}
+    for word in new_paragraph:
+        if word not in banned and word != '':
+            if word not in words:
+                    words[word] = 1
+            else:
+                words[word]+= 1
+    value = max(words.values())
+    
+    for word in words:
+        if words[word] == value:
+            return word
 
 
 
