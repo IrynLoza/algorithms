a14ce5f675311531a5d0d101d499f750df82ea5a
Iryna Loza <irynloza@gmail.com>
2021-02-24 18:13:16 -0800

    Initial commit

---
+++ sum_concatenation.py
@@ -0,0 +1,19 @@
+""" Calculet sum of two strings and oncatenate them using following rules:
+>>> sum_concatenation('99', '99')
+'1818'
+>>> sum_concatenation('12', '7')
+'19'
+>>> sum_concatenation('11', '9')
+'110'
+"""
+
+def sum_concatenation(a, b):
+    pass
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\u2764\uFE0F ALL TESTS PASS! You are doing great!')
