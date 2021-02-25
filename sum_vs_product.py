6d57142cec269418f987063cfc45152359420304
Iryna Loza <irynloza@gmail.com>
2021-02-25 12:02:30 -0800

    Initial commit

---
+++ sum_vs_product.py
@@ -0,0 +1,24 @@
+"""
+Write the function to calculate difference between sum and product of 
+array elements:
+>>> sum_vs_product([1,2,3,4,5]) 
+105
+>>> sum_vs_product([2,4,5,15,8]) 
+4766
+>>> sum_vs_product([1, 1, 1]) 
+-2
+"""
+
+def sum_vs_product(arr):
+    pass
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n ✨Dance with me! ALL TESTS PASS! ✨')
+
+
+
