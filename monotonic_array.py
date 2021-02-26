ee112ee47736992a53bb22fae1e8d40bf232658d
Iryna Loza <irynloza@gmail.com>
2021-02-26 10:29:46 -0800

    Complete is_monotonic

--- monotonic_array.py
+++ monotonic_array.py
@@ -5,20 +5,45 @@ An array A is monotone increasing if for all i <= j, A[i] <= A[j].
 An array A is monotone decreasing if for all i <= j, A[i] >= A[j].
 
 Return true if and only if the given array A is monotonic:
->>> monotonic_array([1,2,2,3])
-true
->>> monotonic_array([6,5,4,4])
-true
->>> monotonic_array([1,3,2])
-false
->>> monotonic_array([1,1,1])
-true
->>> monotonic_array([1,2,4,5])
-true
+>>> is_monotonic([1,2,2,3])
+True
+>>> is_monotonic([6,5,4,4])
+True
+>>> is_monotonic([1,3,2])
+False
+>>> is_monotonic([1,1,1])
+True
+>>> is_monotonic([1,2,4,5])
+True
 """
 
-def monotonic_array(A):
-    pass
+def is_monotonic(A):
+    """Return true if array is monotonic"""
+
+    min_num = A[0]
+    max_num = A[0]
+    
+    for num in A[1:]:
+        if num < min_num:
+            min_num = num
+        if num > max_num:
+            max_num = num
+    
+    if min_num == A[0]:
+        i = 0
+        while i < len(A)-1:
+            if A[i] > A[i+1]:
+                return False
+            i+= 1
+        return True    
+        
+    elif max_num == A[0]: 
+        i = 0
+        while i < len(A)-1:
+            if A[i] < A[i+1]:
+                return False
+            i+= 1
+        return True
 
 
 
