0f6b380984c01c3d793a749cb486e8b5737a6ea6
Iryna Loza <irynloza@gmail.com>
2021-03-01 14:02:24 -0800

    Initial commit

---
+++ fibonacci.py
@@ -0,0 +1,26 @@
+"""The Fibonacci numbers, commonly denoted F(n) form a sequence, called the 
+Fibonacci sequence, such that each number is the sum of the two preceding ones, 
+starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+>>> fib(2)
+1
+>>> fib(3)
+2
+>>> fib(4)
+3
+"""
+
+def fib(n):
+    """Return fibonacci of n"""
+    pass
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n \u2764\uFE0F ALL TESTS PASS! Amazing job!') 
