f38b02a2c9da0f8ca33f337068b1725023c1dd96
Iryna Loza <irynloza@gmail.com>
2021-03-02 16:26:48 -0800

    Initial commit

---
+++ sum_of_unique_elements.py
@@ -0,0 +1,25 @@
+"""You are given an integer array nums. The unique elements of an array are 
+the elements that appear exactly once in the array.
+
+Return the sum of all the unique elements of nums:
+>>> unique_sum([1,2,3,2])
+4
+>>> unique_sum([1,1,1,1])
+0
+>>> unique_sum([1,2,3,4,5])
+15
+>>> unique_sum([1,15,3,3,1,27,8,9,8])
+51
+"""
+
+def unique_sum(nums):
+    """Return the sum of all the unique elements of nums"""
+    pass
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n ✨ALL TESTS PASS! You are a hash maps star!✨')
