8e3bc047d8d15b6df10c217bb04f7d6d613ab85f
Iryna Loza <irynloza@gmail.com>
2021-03-03 13:00:54 -0800

    Initial commit

---
+++ unique_integers_sum_up_to_zero.py
@@ -0,0 +1,22 @@
+"""Given an integer n, return any array containing n unique integers such 
+that they add up to 0.
+>>> sum_zero(5)
+[-2,-1,0,1,2]
+>>> sum_zero(10)
+[-5,-4,-3,-2,-1,1,2,3,4,5]
+>>> sum_zero(3)
+[-1,0,1]
+>>> sum_zero(1)
+[0]
+"""
+
+def sum_zero(n):
+    pass
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n ✨ALL TESTS PASS! Nailed it! ✨')
