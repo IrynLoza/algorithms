cc157fff1d1a43223a0c5d30f4aa31850065f263
Iryna Loza <irynloza@gmail.com>
2021-03-03 13:04:34 -0800

    Complete sum zero

--- unique_integers_sum_up_to_zero.py
+++ unique_integers_sum_up_to_zero.py
@@ -1,17 +1,34 @@
 """Given an integer n, return any array containing n unique integers such 
 that they add up to 0.
 >>> sum_zero(5)
-[-2,-1,0,1,2]
+[-2, -1, 0, 1, 2]
 >>> sum_zero(10)
-[-5,-4,-3,-2,-1,1,2,3,4,5]
+[-5, -4, -3, -2, -1, 1, 2, 3, 4, 5]
 >>> sum_zero(3)
-[-1,0,1]
+[-1, 0, 1]
 >>> sum_zero(1)
 [0]
 """
 
 def sum_zero(n):
-    pass
+    """Return array with sum zero"""
+    if n == 1:
+        return [0]
+    
+    result = []
+    #Take the first num 
+    first = -(n // 2)
+    
+    for i in range(n):
+        #Increase by 1 if 0 and not %
+        if first == 0 and not n%2:
+            first += 1
+        #Add to result    
+        result.append(first)
+        #increase
+        first += 1
+    return result
+    
 
 
 
