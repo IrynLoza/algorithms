1102756fd4061528f1f335633dc97746c3b6909b
Iryna Loza <irynloza@gmail.com>
2021-03-04 20:16:57 -0800

    Complete average_time

--- average_waiting_time.py
+++ average_waiting_time.py
@@ -14,14 +14,31 @@ the input.
 Return the average waiting time of all customers. Solutions within 10-5 from the 
 actual answer are considered accepted:
 >>> average_time([[1,2],[2,5],[4,3]])
-5.00000
+5.0
 >>> average_time([[5,2],[5,4],[10,3],[20,1]])
-3.25000
+3.25
 """
 
 def average_time(customers):
     """Return the average waiting time of all customers"""
-    pass
+    arrival = customers[0][0]
+    first_time = customers[0][1]
+    time = arrival + first_time
+    count_time = time - arrival
+    for i in range(1, len(customers)):
+        #if time greater then arrival of next customer 
+        #increase time by next customer time 
+        if time > customers[i][0]:
+            time += customers[i][1]
+            #increase count_time
+            count_time += time - customers[i][0]
+        else: 
+            #if next customer arrive letter the previous and time less
+            #then arrival -> increase count time by next customer time
+            #and update time with 'first customer formula' 
+            count_time += customers[i][1]
+            time = customers[i][0] + customers[i][1]
+    return count_time/len(customers)
 
 
 
