f87b595adc6bf54549e4b93f325561c3531de096
Iryna Loza <irynloza@gmail.com>
2021-03-05 17:48:14 -0800

    Initial commit

---
+++ richest_customer.py
@@ -0,0 +1,23 @@
+"""You are given an m x n integer grid accounts where accounts[i][j] is the 
+amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that 
+the richest customer has.
+
+A customer's wealth is the amount of money they have in all their bank accounts. 
+The richest customer is the customer that has the maximum wealth.
+>>> richest_customer([[1,2,3],[3,2,1]])
+6
+>>> richest_customer([[1,5],[7,3],[3,5]])
+10
+"""
+
+def richest_customer(accounts):
+    """Return wealth of richest customer"""
+    pass
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+         print('\n \u2764\uFE0F ALL TESTS PASS! Keep going! \u2764\uFE0F')
