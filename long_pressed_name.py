a83246cec9e101857d550bd138edd10461b1a130
Iryna Loza <irynloza@gmail.com>
2021-03-08 10:23:15 -0800

    Complete is_name

--- long_pressed_name.py
+++ long_pressed_name.py
@@ -1,3 +1,4 @@
+import itertools
 """Your friend is typing his name into a keyboard. Sometimes, when typing a 
 character c, the key might get long pressed, and the character will be typed 
 1 or more times.
@@ -17,7 +18,24 @@ True
 
 def is_name(name, typed):
     """Return True if yes, overwise return false"""
-    pass
+    str_list1 = []
+    for key, group in itertools.groupby(name):
+        str_list1.append((key, len(list(group))))
+    #print(str_list1)
+    #[('a', 1), ('l', 1), ('e', 1), ('x', 1)]
+    str_list2 = []
+    for key, group in itertools.groupby(typed):
+        str_list2.append((key, len(list(group))))
+    #print(str_list2)
+    #[('a', 2), ('l', 1), ('e', 2), ('x', 1)]
+    
+    if len(str_list1) != len(str_list2):
+        return False
+    
+    #all() method returns True when all elements in the given iterable are true
+    #zip() function takes iterables (can be zero or more), aggregates them in a tuple, and return it
+    return all(key1 == key2 and value1 <= value2
+                for (key1,value1), (key2,value2) in zip(str_list1, str_list2))
 
 
 
