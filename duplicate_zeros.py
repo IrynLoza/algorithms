e1d8c69912c3dfba8ecc11561ca6e9a75b8d4d5e
Iryna Loza <irynloza@gmail.com>
2021-03-09 14:57:02 -0800

    Initial commit

---
+++ duplicate_zeros.py
@@ -0,0 +1,23 @@
+"""Given a fixed length array arr of integers, duplicate each occurrence of zero, 
+shifting the remaining elements to the right.
+
+Note that elements beyond the length of the original array are not written.
+
+Do the above modifications to the input array in place:
+>>> duplicate_zeros([1,0,2,3,0,4,5,0])
+[1, 0, 0, 2, 3, 0, 0, 4]
+>>> duplicate_zeros([1,2,3])
+[1, 2, 3]
+"""
+
+def duplicate_zeros(nums):
+    """Return array with duplicate zeros"""
+    pass
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n \u2764\uFE0F \u2764\uFE0F \u2764\uFE0F ALL TESTS PASS! Love with you!')
