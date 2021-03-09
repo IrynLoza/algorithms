ec25be28be44116e976b0346774b33d4df17f345
Iryna Loza <irynloza@gmail.com>
2021-03-08 21:26:34 -0800

    Initial commit

---
+++ segments_in_a_string.py
@@ -0,0 +1,23 @@
+"""You are given a string s, return the number of segments in the string. 
+
+A segment is defined to be a contiguous sequence of non-space characters:
+>>> count_segments("Hello, my name is John")
+5
+>>> count_segments("")
+0
+>>> count_segments("love live! mu'sic forever")
+4
+"""
+
+def count_segments(s):
+    """Return number of segments"""
+    pass
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n ✨ALL TESTS PASS! You are shine! ✨')
+
