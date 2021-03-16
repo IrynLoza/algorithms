53b9618b4683f83371c9feb72ba2ec40164c824f
Iryna Loza <irynloza@gmail.com>
2021-03-15 20:06:10 -0700

    Complete add_binary

--- add_binary.py
+++ add_binary.py
@@ -7,7 +7,32 @@
 
 def add_binary(a, b):
     """Return sum in binary"""
-    pass
+    """11 -> 3
+            1 -> 1
+            ==>>> 4 == 100 binary
+              
+    """
+    def to_decimal(num):
+        base = 1
+        devimal_value = 0
+        
+        for i in range(len(num)-1, -1, -1):
+            if num[i] == '1':
+                devimal_value+= base
+            base = base*2
+        return  devimal_value
+    
+    decimal_ab = to_decimal(a) + to_decimal(b)
+    
+    def to_binary(decimal_number):
+        if decimal_number == 0:
+            return 0
+        else:
+            return (decimal_number % 2 + 10 * to_binary(int(decimal_number // 2)))
+        
+    return str(to_binary(decimal_ab))
+
+
 
 
 
