c49bfc27399d0a4d788641945dfc52258b810f88
Iryna Loza <irynloza@gmail.com>
2021-03-15 20:05:22 -0700

    Initial commit

---
+++ add_binary.py
@@ -0,0 +1,18 @@
+"""Given two binary strings a and b, return their sum as a binary string:
+>>> add_binary('11', '1')
+'100'
+>>> add_binary('1010', '1011')
+'10101'
+"""
+
+def add_binary(a, b):
+    """Return sum in binary"""
+    pass
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n ✨ALL TESTS PASS! Only better forward! ✨')
