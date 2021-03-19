a0c9c26acad9083fb95e81a97bdb47843d3b05e2
Iryna Loza <irynloza@gmail.com>
2021-03-19 15:46:47 -0700

    Complete commonChars

--- find_common_characters.py
+++ find_common_characters.py
@@ -5,13 +5,38 @@ not 4 times, you need to include that character three times in the final answer.
 
 You may return the answer in any order:
 >>> commonChars(["bella","label","roller"])
-['e', 'l', 'l'])
+['e', 'l', 'l']
 >>> commonChars(["cool","lock","cook"])
-['c', 'o'])
+['c', 'o']
 """
 
 def commonChars(A):
     """Return common chars"""
+    hash_map = {}
+        
+    #Store letters from the first word
+    for i in range(len(A[0])):
+        if A[0][i] not in hash_map:
+            hash_map[A[0][i]] = 1
+        else:
+            hash_map[A[0][i]]+= 1
+    
+    #Iterate througth each key in hash and rest of words
+    for key in hash_map:
+        for i in range(1,len(A)):
+            #If char from first word in the word
+            if key in A[i]:
+                #Find min between count char from first word and next word
+                hash_map[key] = min(hash_map[key], A[i].count(key))
+            else:
+                #Overwise nullify value and break
+                hash_map[key] = 0
+                break
+    
+    result = []
+    for k,v in hash_map.items():
+        result += v * [k]
+    return result
 
 
 
