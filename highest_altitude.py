1c065ee10ffa904d88c112f187f7e5a5462fac9c
Iryna Loza <irynloza@gmail.com>
2021-03-22 10:10:30 -0700

    Initial commit

---
+++ highest_altitude.py
@@ -0,0 +1,24 @@
+"""There is a biker going on a road trip. The road trip consists of n + 1 
+points at different altitudes. The biker starts his trip on point 0 with 
+altitude equal 0.
+
+You are given an integer array gain of length n where gain[i] is the net 
+gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). Return 
+the highest altitude of a point:
+>>> highest_altitude([-5,1,5,0,-7])
+1
+>>> highest_altitude([-4,-3,-2,-1,4,3,2])
+0
+"""
+
+def highest_altitude(gain):
+    """Return highest altitude"""
+    pass
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n \u2764\uFE0F ALL TESTS PASS! Dance time!')
