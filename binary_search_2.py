a020a7765594a632f9ed7302638cb949199a325c
Iryna Loza <irynloza@gmail.com>
2021-03-22 16:40:00 -0700

    Initial commit

---
+++ binary_search_2.py
@@ -0,0 +1,20 @@
+"""Given an array of integers nums which is sorted in ascending order, 
+and an integer target, write a function to search target in nums. If target 
+exists, then return its index. Otherwise, return -1:
+>>> binary_search([-1,0,3,5,9,12], 9)
+4
+>>> binary_search([-1,0,3,5,9,12], 2)
+-1
+"""
+
+def binary_search(nums, target):
+    """Return index of target element in array or -1 if not exist"""
+    pass
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n \u2764\uFE0F ALL TESTS PASS! One more step to the job!')
