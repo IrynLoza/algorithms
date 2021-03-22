bd3f61de996a5c7981e1977a3916c91123e7fe5b
Iryna Loza <irynloza@gmail.com>
2021-03-22 16:40:40 -0700

    Complete binary_search_2

--- binary_search_2.py
+++ binary_search_2.py
@@ -9,7 +9,22 @@ exists, then return its index. Otherwise, return -1:
 
 def binary_search(nums, target):
     """Return index of target element in array or -1 if not exist"""
-    pass
+    lo = 0
+    hi = len(nums)-1
+    
+    while lo <= hi:
+        mid = (lo+hi) // 2
+        center = nums[mid]
+        
+        if center == target:
+            return mid
+        elif center < target:
+            #target in a rigth side
+            lo = mid+1
+        else:
+            #target in a left side
+            hi = mid-1
+    return -1
 
 
 
