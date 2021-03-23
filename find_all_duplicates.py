38fe9a4d3dbd56179ad64080f9bbc313caff4d21
Iryna Loza <irynloza@gmail.com>
2021-03-23 12:21:45 -0700

    Initial commit

---
+++ find_all_duplicates.py
@@ -0,0 +1,21 @@
+"""Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements 
+appear twice and others appear once.
+
+Find all the elements that appear twice in this array:
+>>> duplicates([4,3,2,7,8,2,3,1])
+[3, 2]
+>>> duplicates([1,3,2,1,8,1,3,1])
+[3]
+"""
+
+def duplicates(nums):
+    """Return all duplicates appear twice"""
+    pass
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n \u2764\uFE0F ALL TESTS PASS! Go go go!')
