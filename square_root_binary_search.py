259266894337568740a02d1401584093b1bb6235
Iryna Loza <irynloza@gmail.com>
2021-03-22 17:03:11 -0700

    Initial commit

---
+++ square_root_binary_search.py
@@ -0,0 +1,24 @@
+"""Find square root of a large number in O(logn) time:
+>>> square_root(10)
+3
+>>> square_root(1000000)
+1000
+>>> square_root(262)
+16
+>>> square_root(1)
+1
+>>> square_root(2)
+1
+"""
+
+def square_root(num):
+    """Return square root of given number"""
+    pass
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n ✨ALL TESTS PASS! Go IRYN! ✨')
