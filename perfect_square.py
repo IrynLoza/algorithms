cbdf742da40450dbc23cea0eb61cc69992d49c2c
Iryna Loza <irynloza@gmail.com>
2021-03-22 17:41:57 -0700

    Initial commit

---
+++ perfect_square.py
@@ -0,0 +1,21 @@
+"""Given a positive integer num, write a function which returns True 
+if num is a perfect square else False.
+
+Follow up: Do not use any built-in library function such as sqrt:
+>>> is_perfect(16)
+True
+>>> is_perfect(14)
+False
+"""
+
+def is_perfect(num):
+    """Return True if square root is perfect or False if not"""
+    pass
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n \u2764\uFE0F ALL TESTS PASS! Higher every time!')
