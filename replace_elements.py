0cdb81c4141e3c692e56ecc458c2ec5c3bacaea5
Iryna Loza <irynloza@gmail.com>
2021-03-25 12:41:19 -0700

    Initial commit

---
+++ replace_elements.py
@@ -0,0 +1,20 @@
+"""Given an array arr, replace every element in that array with the 
+greatest element among the elements to its right, and replace the last 
+element with -1:
+>>> replaced([17,18,5,4,6,1])
+[18, 6, 6, 6, 1, -1]
+>>> replaced([400])
+[-1]
+"""
+
+def replaced(nums):
+    """Return array with replaced numbers"""
+    pass
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n ✨ALL TESTS PASS! Only better forward! ✨')
