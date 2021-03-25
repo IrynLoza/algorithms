9cde850c2755779299fa73b3fe1b4d5ac9d41cd2
Iryna Loza <irynloza@gmail.com>
2021-03-24 22:14:55 -0700

    Initial commit

---
+++ find_first_and_last_position.py
@@ -0,0 +1,23 @@
+"""Given an array of integers nums sorted in ascending order, find the 
+starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1]:
+>>> search_range([5,7,7,8,8,10], 8)
+[3, 4]
+>>> search_range([5,7,7,8,8,10], 6)
+[-1, -1]
+>>> search_range([], 0)
+[-1, -1]
+"""
+
+def search_range(nums, target):
+    """Return first and last position of given element or -1"""
+    pass
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n \u2764\uFE0F ALL TESTS PASS! Did it again! Oh baby, baby!')
