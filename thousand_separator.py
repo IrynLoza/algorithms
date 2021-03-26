2de89bf6b2efb6fb782e3a686f2308fed7a6105a
Iryna Loza <irynloza@gmail.com>
2021-03-26 10:13:00 -0700

    Initial commit

---
+++ thousand_separator.py
@@ -0,0 +1,21 @@
+"""Given an integer n, add a dot (".") as the thousands separator 
+and return it in string format:
+>>> separator(987)
+'987'
+>>> separator(1234)
+'1.234'
+>>> separator(123456789)
+'123.456.789'
+"""
+
+def separator(n):
+    """Return with separator"""
+    pass
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n \u2764\uFE0F ALL TESTS PASS! Did it again! Oh baby, baby!')
