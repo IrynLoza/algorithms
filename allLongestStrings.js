c33d85641cd753eef00e6eb3111b04ba0d395cdd
Iryna Loza <irynloza@gmail.com>
2021-04-02 16:57:49 -0700

    Complete allLongestStrings

--- allLongestStrings.js
+++ allLongestStrings.js
@@ -7,5 +7,21 @@ allLongestStrings(inputArray) = ["aba", "vcd", "aba"]
 */
 
 function allLongestStrings(inputArray) {
-
+    let longestLength = inputArray[0].length;
+    
+    for (let i = 0; i < inputArray.length; i++) {
+        if (inputArray[i].length > longestLength) {
+            longestLength = inputArray[i].length;
+        }
+    }
+    
+    let result = [];
+    
+    for (let i = 0; i < inputArray.length; i++) {
+        if (inputArray[i].length === longestLength) {
+            result.push(inputArray[i]);
+        }
+    }
+    
+    return result;
 }
