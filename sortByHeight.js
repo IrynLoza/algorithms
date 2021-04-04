1a73c34453ab91faef90bba74431d71ca7701c53
Iryna Loza <irynloza@gmail.com>
2021-04-03 19:15:06 -0700

    Complete sortByHeight

--- sortByHeight.js
+++ sortByHeight.js
@@ -10,5 +10,44 @@ sortByHeight(a) = [-1, 150, 160, 170, -1, -1, 180, 190].
 */
 
 function sortByHeight(a) {
+    // let trees = [];
+    // let people = [];
+    
+    // for (let i = 0; i < a.length; i++) {
+    //     if (a[i] === -1) {
+    //         trees.push(i);
+    //     } else {
+    //         people.push(a[i]);
+    //     }  
+    // }
+    // console.log(people)
+    // people = people.sort();
+    
+    
+    // for (let i = 0; i < people.length; i++) {
+    //     people.splice(trees[i], 0, -1);
+        
+    // }
+
+    // return people
+    
+    var treePos = [];
+    var  heights = [];
+    for(var i = 0; i < a.length; i++) {
+      if(a[i] === -1) {
+          treePos.push(i);
+      } else {
+          heights.push(a[i]);
+      }
+    }
+    var sortedHeights = heights.sort(function(aa, bb) {
+        return aa - bb;
+    });
+    for(var j = 0; j < a.length; j++) {
+      if(treePos.indexOf(j) !== - 1) {
+        sortedHeights.splice(j, 0, -1);
+      }
+    }
+    return sortedHeights;
     
 }
