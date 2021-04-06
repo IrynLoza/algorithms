5304ddb1f0f78431da77d00040f3dc46aff24024
Iryna Loza <irynloza@gmail.com>
2021-04-05 20:58:20 -0700

    Initial commit

---
+++ firstNotRepeatingCharacter.js
@@ -0,0 +1,20 @@
+/*
+Given a string s consisting of small English letters, find and return the 
+first instance of a non-repeating character in it. If there is no such 
+character, return '_'.
+
+Example
+
+For s = "abacabad", the output should be
+firstNotRepeatingCharacter(s) = 'c'.
+
+There are 2 non-repeating characters in the string: 'c' and 'd'. Return c since 
+it appears in the string first.
+*/
+
+function firstNotRepeatingCharacter(s) {
+
+}
+
+console.log(firstNotRepeatingCharacter("abacabad")) //'c'
+console.log(firstNotRepeatingCharacter("abacabaabacaba")) //'_'
