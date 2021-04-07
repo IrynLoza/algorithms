a2e4d7ab4b01fe661769a71af1b45abfaed9f8d8
Iryna Loza <irynloza@gmail.com>
2021-04-06 18:40:01 -0700

    Initial commit

---
+++ nextGreaterElementI.js
@@ -0,0 +1,17 @@
+/*
+You are given two integer arrays nums1 and nums2 both of unique elements, 
+where nums1 is a subset of nums2.
+
+Find all the next greater numbers for nums1's elements in the corresponding 
+places of nums2.
+
+The Next Greater Number of a number x in nums1 is the first greater number to 
+its right in nums2. If it does not exist, return -1 for this number.
+*/
+
+function nextGreater(nums1, nums2) {
+
+}
+
+console.log(nextGreater([4,1,2], [1,3,4,2])) // Output: [-1,3,-1]
+console.log(nextGreater([2,4], [1,2,3,4])) // [3, -1]
