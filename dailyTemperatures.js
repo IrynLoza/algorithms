630a93e3833dc38606dda2012605a442e16924a7
Iryna Loza <irynloza@gmail.com>
2021-04-07 17:20:05 -0700

    Complete dailyTemperature

--- dailyTemperatures.js
+++ dailyTemperatures.js
@@ -9,7 +9,23 @@ will be an integer in the range [30, 100].
 */
 
 function dailyTemperature(T) {
-
+    let stack = [];
+    
+    for (let i = 0; i < T.length; i++) {
+        stack.push(0);
+        let count = 0;
+        for (let y = i; y < T.length; y ++) {
+            if (T[y] > T[i]) {
+                stack.pop();
+                stack.push(count);
+                break;
+            } else {
+                count++;
+            }
+        }
+    }
+    
+    return stack;
 };
 
 console.log(dailyTemperature([73, 74, 75, 71, 69, 72, 76, 73])); 
