7cba5ff245b29ab65057c197281b4b10dc433163
Iryna Loza <irynloza@gmail.com>
2021-04-09 00:02:08 -0700

    Initial commit

---
+++ addBorder.js
@@ -0,0 +1,9 @@
+/*
+Given a rectangular matrix of characters, add a border of asterisks(*) to it.
+*/
+
+function addBorder(picture) {
+
+}
+
+console.log(addBorder(["abc", "ded"])); //["*****","*abc*","*ded*","*****"]
