349f6e20017beaa854baccf482c002e6f634a366
Iryna Loza <irynloza@gmail.com>
2021-04-08 23:47:24 -0700

    Complete revearseInParentheses

--- reverseInParentheses.js
+++ reverseInParentheses.js
@@ -6,7 +6,26 @@ Input strings will always be well-formed with matching ()s.
 */
 
 function revearseInParentheses(inputString) {
+    inputString = inputString.split('');
+    let stack = [];
+    let temp = [];
 
+    for (let i = 0; i < inputString.length; i++) {
+        if (inputString[i] === '(') {
+            stack.push(i);
+        }
+        
+        if (inputString[i] === ')') {
+            let firstIndex = stack.pop();
+            let lastIndex = i;
+            temp = inputString.slice(firstIndex, lastIndex + 1);
+            temp.reverse();
+            inputString.splice(firstIndex, temp.length, ...temp);
+            
+        } 
+    }
+    const result = inputString.join('').replace(/[()]/gi, '');
+    return result;
 }
 
 console.log(revearseInParentheses("(bar)")); // 'rab'
