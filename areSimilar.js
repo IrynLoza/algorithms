827ea931d90d160fa458e7145435553ea5803c7b
Iryna Loza <irynloza@gmail.com>
2021-04-10 21:26:57 -0700

    Initial commit

---
+++ areSimilar.js
@@ -0,0 +1,14 @@
+/*
+Two arrays are called similar if one can be obtained from another by swapping 
+at most one pair of elements in one of the arrays.
+
+Given two arrays a and b, check whether they are similar.
+*/
+
+function areSimilar(a,b) {
+
+}
+
+console.log(areSimilar([1, 2, 3], [1, 2, 3])) // true
+console.log(areSimilar([1, 2, 3], [2, 1, 3])) // true
+console.log(areSimilar([1, 2, 2], [2, 1, 1])) // false
