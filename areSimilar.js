e4faa3895ddfc7a3757146eee4c20d2b966984f5
Iryna Loza <irynloza@gmail.com>
2021-04-11 21:20:37 -0700

    Complete areSimilar

--- areSimilar.js
+++ areSimilar.js
@@ -6,7 +6,27 @@ Given two arrays a and b, check whether they are similar.
 */
 
 function areSimilar(a,b) {
+    if (a.toString() === b.toString()) {
+        return true;
+    }
+   
+    let c = [];
+    let d = []; 
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] !== b[i]) {
+            c.push(a[i]);
+            d.push(b[i]);
+        }
+    } 
+    
+    d = d.reverse();
 
+    
+    if (c.length === 2 && (c.toString() === d.toString())) {
+        return true;
+    }
+    
+    return false;
 }
 
 console.log(areSimilar([1, 2, 3], [1, 2, 3])) // true
