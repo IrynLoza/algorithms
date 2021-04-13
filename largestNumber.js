d66b1bf0c1cb2fc6aead2309ff124e77246c1251
Iryna Loza <irynloza@gmail.com>
2021-04-13 11:47:41 -0700

    Initial commit

---
+++ largestNumber.js
@@ -0,0 +1,10 @@
+/*
+Given an integer n, return the largest number that contains exactly n digits.
+*/
+
+function largestNum(n) {
+
+}
+
+console.log(largestNum(2)) // 99
+console.log(largestNum(3)) // 999
