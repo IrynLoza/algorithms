9ac893d3699b859586229b0cdd0ebf9969ef59dc
Iryna Loza <irynloza@gmail.com>
2021-04-12 19:17:24 -0700

    Initial commit

---
+++ kthMissingPositiveNumber.js
@@ -0,0 +1,13 @@
+/*
+Given an array arr of positive integers sorted in a strictly increasing order, 
+and an integer k.
+
+Find the kth positive integer that is missing from this array.
+*/
+
+function kthMissing(arr, k) {
+
+}
+
+console.log(kthMissing([2,3,4,7,11], 5)) // 9
+console.log(kthMissing([1,2,3,4], 2)) // 6
