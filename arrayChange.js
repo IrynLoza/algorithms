b32c2f9175f7e18fcdae7761e83db4fbd01db4b8
Iryna Loza <irynloza@gmail.com>
2021-04-12 21:19:50 -0700

    Initial commit

---
+++ arrayChange.js
@@ -0,0 +1,13 @@
+/**
+You are given an array of integers. On each move you are allowed to increase 
+exactly one of its element by one. Find the minimal number of moves required 
+to obtain a strictly increasing sequence from the input. 
+*/
+
+function arrayChange(inputArray) {
+
+}
+
+console.log(arrayChange([1,1,1])) // 3
+console.log(arrayChange([-1000, 0, -2, 0])) // 5
+console.log(arrayChange([2, 1, 10, 1])) // 12
