6fc5d0e5ca11e5d7f05d12eaac613090b8941b76
Iryna Loza <irynloza@gmail.com>
2021-04-12 21:49:23 -0700

    Initial commit

---
+++ arrayMaximalAdjacentDifference.js
@@ -0,0 +1,13 @@
+/*
+Given an array of integers, find the maximal absolute difference
+ between any two of its adjacent elements.
+*/
+
+function arrayMaximalAdjacentDifference(inputArray) {
+
+}
+
+console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0])) //3
+console.log(arrayMaximalAdjacentDifference([10, 11, 13])) //2
+console.log(arrayMaximalAdjacentDifference([-2, -2, -2, -2, -2])) //0
+console.log(arrayMaximalAdjacentDifference([-1, 1, -3, -4])) //4
