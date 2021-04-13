530d4c577ed18d90a4adcd4b492eb07736ddf5af
Iryna Loza <irynloza@gmail.com>
2021-04-12 21:27:32 -0700

    Initial commit

---
+++ palindromeRearranging.js
@@ -0,0 +1,12 @@
+/* 
+Given a string, find out if its characters can be rearranged to form a palindrome.
+*/
+
+function isPalindrome(inputString) {
+
+}
+
+console.log(isPalindrome('aabb')) //true
+console.log(isPalindrome('zyyzzzzz')) //true
+console.log(isPalindrome('abca')) //false
+
