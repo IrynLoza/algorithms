1455174407bd7f9a14f96cdb445c065db817f142
Iryna Loza <irynloza@gmail.com>
2021-04-13 10:04:14 -0700

    Complete addTwoDigits

--- addTwoDigits.js
+++ addTwoDigits.js
@@ -3,6 +3,8 @@ You are given a two-digit integer n. Return the sum of its digits.
 */
 
 function addTwoDigits(n) {
-
+    n = n.toString();
+    return +n[0]+(+n[1])
 }
-
+console.log(addTwoDigits(29)) //11
+console.log(addTwoDigits(32)) //5
