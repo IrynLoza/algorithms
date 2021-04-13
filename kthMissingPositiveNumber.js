7d4e63175a48f8eac069020e1cf3f5d5b20b2dbc
Iryna Loza <irynloza@gmail.com>
2021-04-12 19:18:00 -0700

    Complete kthMissing

--- kthMissingPositiveNumber.js
+++ kthMissingPositiveNumber.js
@@ -6,7 +6,20 @@ Find the kth positive integer that is missing from this array.
 */
 
 function kthMissing(arr, k) {
-
+    let missing = [];
+    let curDigit = 1;
+    i = 0
+    while (missing.length < k) {
+        if (arr[i] === curDigit) {
+            i++
+            curDigit++
+        } else {
+            missing.push(curDigit)
+            curDigit++
+        }
+    }
+    
+    return missing[missing.length-1]
 }
 
 console.log(kthMissing([2,3,4,7,11], 5)) // 9
