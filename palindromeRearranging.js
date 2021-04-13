c87127dc90439a2ac5265da089f051f6748a7211
Iryna Loza <irynloza@gmail.com>
2021-04-12 21:28:17 -0700

    Complete isPalindrome

--- palindromeRearranging.js
+++ palindromeRearranging.js
@@ -3,7 +3,28 @@ Given a string, find out if its characters can be rearranged to form a palindrom
 */
 
 function isPalindrome(inputString) {
-
+    let count = 0;
+    let hash_map = {};
+    
+    for (let i = 0; i < inputString.length; i++) {
+        if (inputString[i] in hash_map) {
+            hash_map[inputString[i]]++;
+        } else {
+            hash_map[inputString[i]] = 1;
+        }
+    }
+    
+    for (let key in hash_map) {
+        if (hash_map[key]%2 !== 0) {
+            count++;
+        }
+    }
+    
+    if (count > 1) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 console.log(isPalindrome('aabb')) //true
