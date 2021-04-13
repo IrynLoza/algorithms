eb4a072ed45f5c035447fd376c1365685f7e73d4
Iryna Loza <irynloza@gmail.com>
2021-04-12 21:20:43 -0700

    Complete arrayChange

--- arrayChange.js
+++ arrayChange.js
@@ -5,7 +5,18 @@ to obtain a strictly increasing sequence from the input.
 */
 
 function arrayChange(inputArray) {
-
+    let count = 0;
+    
+    for (let i = 0; i < inputArray.length; i++) {
+        if (inputArray[i] >= inputArray[i+1]) {
+            while (inputArray[i] >= inputArray[i+1]) {
+                inputArray[i+1]++;
+                count++;
+            }
+        }
+    }
+    
+    return count;
 }
 
 console.log(arrayChange([1,1,1])) // 3
