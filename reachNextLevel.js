a1dd63b3354f14f5bf9e4d7c306551ddef46ca72
Iryna Loza <irynloza@gmail.com>
2021-04-15 17:07:53 -0700

    Initial commit

---
+++ reachNextLevel.js
@@ -0,0 +1,17 @@
+/*
+You are playing an RPG game. Currently your experience points (XP) total is 
+equal to experience. To reach the next level your XP should be at least at 
+threshold. If you kill the monster in front of you, you will gain more 
+experience points in the amount of the reward.
+
+Given values experience, threshold and reward, check if you reach the next 
+level after killing the monster.
+*/
+
+function reachNextLevel(experience, threshold, reward) {
+
+}
+
+console.log(reachNextLevel(10, 15, 5)) //true
+console.log(reachNextLevel(5, 10, 5)) //true
+console.log(reachNextLevel(5, 15, 5)) //false
