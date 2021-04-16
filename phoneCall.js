fc4eb0323d05c670653c093b29d20a3338652b40
Iryna Loza <irynloza@gmail.com>
2021-04-15 17:03:42 -0700

    Complete phoneCall

--- phoneCall.js
+++ phoneCall.js
@@ -10,7 +10,27 @@ you can have?
 */
 
 function phoneCall(min1, min2_10, min11, s) {
-
+    let minutes = 0;
+  
+    let rate = min1;
+    
+    while (s > 0) {
+        
+        minutes += 1;
+        
+        if (minutes == 2) {
+            rate = min2_10;
+        } else if (minutes > 10) {
+            rate = min11;
+        }
+        
+        s -= rate
+        if (s < 0) {
+            minutes -= 1;
+        }
+        
+    }    
+    return minutes;
 }
 
 console.log(phoneCall(3,1,2,20)) //14
