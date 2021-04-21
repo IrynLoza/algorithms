5550ef6aff5931317aaf9d4a44cc539e5b7cb446
Iryna Loza <irynloza@gmail.com>
2021-04-20 17:22:05 -0700

    Initial commit

---
+++ knapsackLight.js
@@ -0,0 +1,18 @@
+/*
+You found two items in a treasure chest! The first item weighs weight1 
+and is worth value1, and the second item weighs weight2 and is worth value2. 
+What is the total maximum value of the items you can take with you, assuming 
+that your max weight capacity is maxW and you can't come back for the items 
+later?
+
+Note that there are only two items and you can't bring more than one item 
+of each type, i.e. you can't take two first items or two second items.
+*/
+
+function knapsackLight(value1, weight1, value2, weight2, maxW) {
+    
+}
+
+console.log(knapsackLight(10,5,6,4,8)) //10
+console.log(knapsackLight(5,3,7,4,6)) //7
+console.log(knapsackLight(10,2,11,3,1)) //0
