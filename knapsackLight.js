cce570d4084e918cecd5433e4677c7e7d70044b8
Iryna Loza <irynloza@gmail.com>
2021-04-20 17:22:37 -0700

    Complete knapsackLight

--- knapsackLight.js
+++ knapsackLight.js
@@ -10,7 +10,21 @@ of each type, i.e. you can't take two first items or two second items.
 */
 
 function knapsackLight(value1, weight1, value2, weight2, maxW) {
+    if (weight1 + weight2 <= maxW) {
+        return value1 + value2;
+    } else if (weight1 > maxW && weight2 > maxW) {
+        return 0;
+    } else if (weight1 <= maxW && weight2 > maxW) {
+        return value1;
+    } else if (weight2 <= maxW && weight1 > maxW) {
+        return value2;
+    } else if (value1 >= value2) {
+        return value1;
+    } else if (value1 < value2) {
+        return value2;
+    }
     
+    return 0;
 }
 
 console.log(knapsackLight(10,5,6,4,8)) //10
