b01a1f3fb338edf3e5ceab5f1a1cb33a8e687b0a
Iryna Loza <irynloza@gmail.com>
2021-04-20 17:28:31 -0700

    Initial commit

---
+++ extraNumber.js
@@ -0,0 +1,13 @@
+/*
+You're given three integers, a, b and c. It is guaranteed that 
+two of these integers are equal to each other. What is the value 
+of the third integer?
+*/
+
+function extraNumber(a, b, c) {
+
+};
+
+console.log(extraNumber(2,7,2)); //7
+console.log(extraNumber(5,5,2)); //2
+console.log(extraNumber(4,2,2)); //4
