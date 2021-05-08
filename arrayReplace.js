415e4a35e41786b087ec290fea8b9c275bc9577f
Iryna Loza <irynloza@gmail.com>
2021-05-07 17:15:23 -0700

    Complete arrayReplace

--- arrayReplace.js
+++ arrayReplace.js
@@ -4,7 +4,12 @@ with substitutionElem.
 */
 
 function arrayReplace(inputArray, elemToReplace, substitutionElem) {
-
+    for (let i = 0; i < inputArray.length; i++) {
+        if (inputArray[i] === elemToReplace) {
+           inputArray[i] = substitutionElem;
+        }
+    }
+    return inputArray;
 }
 
 console.log(arrayReplace([1,2,1], 1, 3)); //[3,2,3]
