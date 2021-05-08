6c2bb9f9da5e9f9b287e8d5cfbbf92a9e8cf9014
Iryna Loza <irynloza@gmail.com>
2021-05-07 17:14:46 -0700

    Initial commit

---
+++ arrayReplace.js
@@ -0,0 +1,12 @@
+/*
+Given an array of integers, replace all the occurrences of elemToReplace 
+with substitutionElem.
+*/
+
+function arrayReplace(inputArray, elemToReplace, substitutionElem) {
+
+}
+
+console.log(arrayReplace([1,2,1], 1, 3)); //[3,2,3]
+console.log(arrayReplace([1, 2, 3, 4, 5], 3, 0)); //[1, 2, 0, 4, 5]
+console.log(arrayReplace([1, 2, 1, 2, 1], 2, 2)); //[1, 2, 1, 2, 1]
