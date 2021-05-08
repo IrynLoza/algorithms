fb3f48c445c62e51f43281de108a11f6b1f23a13
Iryna Loza <irynloza@gmail.com>
2021-05-07 17:23:20 -0700

    Complete firstReverseTry

--- firstReverseTry.js
+++ firstReverseTry.js
@@ -9,7 +9,13 @@ array.
 */
 
 function firstReverseTry(arr) {
+    if (arr.length <= 0) {
+        return [];
+    }
 
+    [arr[0], arr[arr.length-1]] = [arr[arr.length-1], arr[0]];
+    
+    return arr;
 }
 
 console.log(firstReverseTry([1, 2, 3, 4, 5])); //[5, 2, 3, 4, 1]
