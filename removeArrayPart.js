ff499beb7bf30cdfe9c0296c5088ddcad8be6ab6
Iryna Loza <irynloza@gmail.com>
2021-05-10 11:04:40 -0700

    Initial commit

---
+++ removeArrayPart.js
@@ -0,0 +1,12 @@
+/*
+Remove a part of a given array between given 0-based indexes 
+l and r (inclusive).
+*/
+
+function removeArrayPart(inputArray, l, r) {
+
+}
+
+console.log(removeArrayPart([2, 3, 2, 3, 4, 5], 2, 4)); // [2,3,5]
+console.log(removeArrayPart([2, 4, 10, 1], 0, 2)); // [1]
+console.log(removeArrayPart([1, 1], 0, 1)); // []
