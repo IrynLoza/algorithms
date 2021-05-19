57755bbde6005577a74e1081d12e4d4bab1b0d98
Iryna Loza <irynloza@gmail.com>
2021-05-18 20:19:11 -0700

    Initial commit

---
+++ isPower.js
@@ -0,0 +1,12 @@
+/*
+Determine if the given number is a power of some non-negative integer.
+*/
+
+function isPower(n) {
+    
+    
+}
+
+console.log(isPower(125)) //true
+console.log(isPower(72)) //false
+console.log(isPower(11)) //false
