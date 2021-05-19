ce103a288a9c4d06f21abd2a23488cc26bf8ded8
Iryna Loza <irynloza@gmail.com>
2021-05-19 16:48:17 -0700

    Initial commit

---
+++ isSumOfConsecutive2.js
@@ -0,0 +1,25 @@
+/*
+Find the number of ways to express n as sum of some (at least two) consecutive positive 
+integers.
+
+Example
+
+For n = 9, the output should be
+isSumOfConsecutive2(n) = 2.
+There are two ways to represent n = 9: 2 + 3 + 4 = 9 and 4 + 5 = 9.
+
+For n = 8, the output should be
+isSumOfConsecutive2(n) = 0.
+There are no ways to represent n = 8.
+*/
+
+function isSumOfConsecutive2(n) {
+
+}
+
+console.log(isSumOfConsecutive2(9)) //2
+console.log(isSumOfConsecutive2(8)) //0
+console.log(isSumOfConsecutive2(8)) //0
+console.log(isSumOfConsecutive2(24)) //1
+console.log(isSumOfConsecutive2(25)) //2
+console.log(isSumOfConsecutive2(880)) //3
