6ae6f5446b71891102d054e4597424ff492e258c
Iryna Loza <irynloza@gmail.com>
2021-05-24 21:11:43 -0700

    Complete comfortableNumbers

--- comfortableNumbers.js
+++ comfortableNumbers.js
@@ -8,8 +8,26 @@ feels comfortable with a)?
 */
 
 function comfortableNumbers(l, r) {
-
+    let pairs = 0;
+    for (i=l;i<=r;i++) {
+        for (j=i+1;j<=r;j++) {
+            if (i+sumDigits(i) >= j && j-sumDigits(j)<=i) {
+                pairs++;
+            }
+        }
+    }
+    return pairs;
 }
+
+function sumDigits(n) {
+    let sum = 0;
+    while (n) {
+        sum += n % 10;
+        n = Math.floor(n / 10);
+    }
+    return sum;
+}
+
 
 console.log(comfortableNumbers(10, 12)) //2
 console.log(comfortableNumbers(1, 9)) //20
