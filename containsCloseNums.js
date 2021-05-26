5b543ecbf8e2d99171e44b6ae42d49cfcd9e40c3
Iryna Loza <irynloza@gmail.com>
2021-05-25 22:14:40 -0700

    Complete containsCloseNums

--- containsCloseNums.js
+++ containsCloseNums.js
@@ -14,7 +14,29 @@ containsCloseNums(nums, k) = false.
 */
 
 function containsCloseNums(nums, k) {
+    let flag = false;
+    hashMap = {}
     
+    for (let i = 0; i < nums.length; i++) {
+        if (!(nums[i] in hashMap)) {
+            hashMap[nums[i]] = [i];
+        } else {
+            hashMap[nums[i]].push(i);
+        }
+    }
+
+    for (let key in hashMap) {
+        if (hashMap[key].length >= 2) {
+            for (let j = 0; j < hashMap[key].length-1; j++) {
+                if (hashMap[key][j+1] - hashMap[key][j] <= k) {
+                    return true;
+                } else {
+                    flag = false;
+                }
+            }
+        } 
+    }
+    return flag;
 }
 
 console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 3)); //true
