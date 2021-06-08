d458aed26bd4f38c7bc84d1d8887cc4be531ff54
Iryna Loza <irynloza@gmail.com>
2021-06-07 21:22:26 -0700

    Initial commit

---
+++ network.js
@@ -0,0 +1,11 @@
+/*
+input [[5, ON], [7, OFF], [30, ON], [32, OFF], [34, ON]]
+output [[5,7], [30,32]]
+*/
+
+function findOffTimeStamps(arr) {
+    
+}
+
+console.log(findOffTimeStamps([[5, ON], [7, OFF], [30, ON], [32, OFF], [34, ON]])) 
+// [[5,7], [30,32]]
