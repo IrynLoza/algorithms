4fe81f7780d4e50b8269ca99a7fc8194f2921022
Iryna Loza <irynloza@gmail.com>
2021-07-14 17:58:58 -0700

    Complete reverseInteger

--- reverseInteger.js
+++ reverseInteger.js
@@ -3,7 +3,19 @@ Reverse the digits of the given integer.
 */
 
 function reverseInteger(x) {
-
+    const string = x.toString().split('');
+    let result = [];
+    if (string[0] === '-') {
+        result.push('-');
+        string.splice(0, 1);
+    }
+    
+    for (let i = string.length-1; i >= 0; i--) {
+        result.push(string[i]);
+    }
+    const arr = result.join('');
+    
+    return +arr;
 };
 
 console.log(reverseInteger(12345)) //54321
