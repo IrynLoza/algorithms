9d688ad8b9d840ed1dbd526de33725f1422c42eb
Iryna Loza <irynloza@gmail.com>
2021-07-15 17:14:05 -0700

    Initial commit

---
+++ missingNumber.js
@@ -0,0 +1,15 @@
+/*
+You are supposed to label a bunch of boxes with numbers from 0 to n, 
+and all the labels are stored in the array arr. Unfortunately one of 
+the labels is missing. Your task is to find it.
+*/
+
+function missingNumber(arr) {
+
+};
+
+console.log(missingNumber([3,1,0])) //2
+console.log(missingNumber([0])) //1
+console.log(missingNumber([5, 2, 1, 6, 3, 0])) //4
+
+
